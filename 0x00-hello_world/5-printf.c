635491c2134c96ff6d0441ce362b1e3a23165424
brayohmurithidev <brianmurithim@gmail.com>
2022-06-02 16:17:50 -0700

    prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Descriptio: main- prints sring stdout
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
