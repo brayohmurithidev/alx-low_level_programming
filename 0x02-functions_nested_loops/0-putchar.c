5d3594f8e43980cbf5c759c8978857a288ba42fc
brayohmurithidev <brianmurithim@gmail.com>
2022-06-07 02:42:12 -0700

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -program to print _putchar followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	char str_hbt[9] = "Holberton";
+
+	while (a < 9)
+	{
+		_putchar(str_hbt[a]);
+		a += 1;
+	}
+	_putchar('\n');
+	return (0);
+}
