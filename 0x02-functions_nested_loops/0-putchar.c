eef6fdec7fc2fe9a159f55d33d70fa4fc62e0e52
brayohmurithidev <brianmurithim@gmail.com>
2022-06-07 04:53:15 -0700

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 	int a = 0;
-	char str_hbt[9] = "_putchar";
+	char text[9] = "_putchar";
 
 	for (a = 0; 0 < 8; a++)
 	{
-		_putchar(str_hbt[a]);
+		_putchar(text[a]);
 	}
 	_putchar('\n');
 	return (0);
