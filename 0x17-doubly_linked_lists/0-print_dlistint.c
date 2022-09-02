fd7a4d41e568a67084c1d9a3066692e67c30a4e6
brayohmurithidev <murithibrianm@gmail.com>
2022-09-02 23:39:34 +0300

    Mega push

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,40 +1,27 @@
 #include "lists.h"
-
-
+#include <stdio.h>
 
 /**
- * print_dlistint - prints a doubly linked list
- * @h: pointer to the list
- * Return: number of nodes in the list
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: a pointer to the head of the list
+ *
+ * Return: the number of nodes in the list
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
-
-	size_t nodes = 0;
-
-
+	size_t n_nodes = 0;
 
 	if (!h)
-
 		return (0);
-
-
+	while (h->prev)
+		h = h->prev;
 
 	while (h)
-
 	{
-
 		printf("%d\n", h->n);
-
+		n_nodes++;
 		h = h->next;
-
-		nodes++;
-
 	}
 
-
-
-	return (nodes);
-
+	return (n_nodes);
 }
