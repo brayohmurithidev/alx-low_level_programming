f150cbeac8a841730388b6beb3e475f4aa9b5f90
brayohmurithidev <murithibrianm@gmail.com>
2022-09-05 23:19:51 +0300

    dynamic libs

--- 0x18-dynamic_libraries/atoi.c
+++ 0x18-dynamic_libraries/atoi.c
@@ -12,7 +12,9 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	do {
+	do 
+	{
+
 		if (*s == '-')
 			sign *= -1;
 
@@ -22,7 +24,8 @@ int _atoi(char *s)
 		else if (num > 0)
 			break;
 
-	} while (*s++);
+	}
+	while (*s++);
 
-	return (num * sign);
+	return (num *sign);
 }
